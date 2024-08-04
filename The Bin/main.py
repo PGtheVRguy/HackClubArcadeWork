fccb93cf341baa81936e8ba7cd63a77105157e98
PGtheVRguy <46075828+PGtheVRguy@users.noreply.github.com>
2024-08-04 13:48:01 -0400

    The Bin rough prototype finished

--- The Bin/main.py
+++ The Bin/main.py
@@ -2,7 +2,7 @@ from machine import Pin, I2C
 from ssd1306 import SSD1306_I2C
 import ds1307
 import utime
-from rotary_encoder import RotaryEncoder  # Import the RotaryEncoder class
+from rotary_encoder import RotaryEncoder  
 
 # Constants
 PIX_RES_X = 128
@@ -14,6 +14,8 @@ ENCODER_B_PIN = 1  # GPIO1
 BUTTON_PIN = 2     # GPIO2
 
 changingTime = False
+alarm_time = [0, 0, 0]  # [hour, minute, second]
+alarmSet = False
 
 def get_date_string(ds1307rtc):
     dt = ds1307rtc.datetime
@@ -37,28 +39,88 @@ def init_i2c(scl_pin, sda_pin):
     return i2c_dev
 
 def display_time(oled, ds1307rtc, encoder):
-    global changingTime
+    global changingTime, alarm_time, alarmSet
+    
     while True:
         oled.fill(0)
         date_string = get_date_string(ds1307rtc)
         time_string = get_time_string(ds1307rtc)
-        encoder_position = encoder.get_position()
-
+        
         oled.text(date_string, 5, 5)
         center_text(oled, time_string, 50, 25)
-        #oled.text(f"Encoder: {encoder_position}", 5, 45)
+        
         if encoder.is_button_pressed():
             changingTime = not changingTime
+            encoder.reset()  # Reset the encoder position when toggling changingTime
+            if changingTime:
+                at = ds1307rtc.datetime
+                alarm_time = [at[3], at[4], at[5]]
+            else:
+                alarmSet = True
 
         if changingTime:
-            oled.text("CHANGING TIME", 5, 45)
+            oled.text("SETTING ALARM", 5, 45)
+            encoder_position = encoder.get_position()
+            print(f"Encoder position: {encoder_position}")  # Debugging print
+            alarm_time[2] += encoder_position
+            if alarm_time[2] >= 60:
+                alarm_time[2] = 0
+                alarm_time[1] += 1
+            elif alarm_time[2] < 0:
+                alarm_time[2] = 59
+                alarm_time[1] -= 1
+
+            encoder.reset()  
+
+            oled.text(f"{alarm_time[0]:02d}:{alarm_time[1]:02d}:{alarm_time[2]:02d}", 5, 55)
+        
+        if alarmSet:
+            oled.text(f"{alarm_time[0]:02d}:{alarm_time[1]:02d}:{alarm_time[2]:02d}", 5, 55)
+            ct = ds1307rtc.datetime
+            if (ct[3] == alarm_time[0] and ct[4] == alarm_time[1] and ct[5] >= alarm_time[2]):
+                while alarmSet:
+                    oled.fill(0)
+                    center_text(oled, "!!ALARM!!", 50, 25)
+                    oled.show()
+                    if encoder.is_button_pressed():
+                        alarmSet = False
+                    utime.sleep(0.05)
         
         oled.show()
-        utime.sleep(0.2)  # Update every second
+        utime.sleep(0.2)
 
 def center_text(oled, text, _x, _y, text_width=8):
     offset = (len(text) * text_width) // 2
     oled.text(text, _x - (offset) // 2, _y)
+
+class RotaryEncoder:
+    def __init__(self, pin_a, pin_b, button_pin):
+        self.pin_a = Pin(pin_a, Pin.IN, Pin.PULL_UP)
+        self.pin_b = Pin(pin_b, Pin.IN, Pin.PULL_UP)
+        self.button = Pin(button_pin, Pin.IN, Pin.PULL_UP)
+        self.position = 0
+        self.last_state = self.pin_a.value()
+
+        self.pin_a.irq(trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING, handler=self.update)
+
+    def update(self, pin):
+        state = self.pin_a.value()
+        if state != self.last_state:
+            if self.pin_b.value() != state:
+                self.position += 1
+            else:
+                self.position -= 1
+        self.last_state = state
+
+    def get_position(self):
+        pos = self.position
+        return pos
+
+    def reset(self):
+        self.position = 0
+
+    def is_button_pressed(self):
+        return not self.button.value()
 
 def main():
     global changingTime
@@ -71,12 +133,10 @@ def main():
     ds1307rtc = ds1307.DS1307(i2c_rtc, 0x68)
     
     print("Initializing Rotary Encoder...")
-    encoder = RotaryEncoder(ENCODER_A_PIN, ENCODER_B_PIN)
+    encoder = RotaryEncoder(ENCODER_A_PIN, ENCODER_B_PIN, BUTTON_PIN)
     
-
-    print("Displaying current time and encoder position...")
+    print("Displaying current time")
     display_time(oled, ds1307rtc, encoder)
-    
 
 if __name__ == '__main__':
     main()
